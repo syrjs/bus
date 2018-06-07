2df3693212a8f690fd44fe8eb0bd78ac6ea21b29
Derek Anderson <dereanderson@paypal.com>
2018-06-07 01:14:29 -0700

    update package

--- app2.js
+++ app2.js
@@ -1,16 +1,16 @@
 import { Client } from '@syr/bus';
 
-// create a new for this app client, with an endpoint
+// create a new client for this app client, with an endpoint
 let client = new Client('com.derek.mathApp');
 
 // register a method for this app
 client.fn('double', ({ num }) => {
-  document.body.innerHTML = `double ${num}<br>${document.body.innerHTML}`
+  document.body.innerHTML = `double ${num} => ${num * 2}<br>${document.body.innerHTML}`
   return num * 2;
 });
 
 // register a method for this app
 client.fn('halve', ({ num }) => {
-  document.body.innerHTML = `halve ${num}<br>${document.body.innerHTML}`
+  document.body.innerHTML = `halve ${num} => ${num / 2}<br>${document.body.innerHTML}`
   return num / 2;
 });
